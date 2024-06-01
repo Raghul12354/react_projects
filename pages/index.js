6f8cb460ba6df175af50ceb17e0bb3417b0b07c5
raghul <raghulkrishna50@gmail.com>
2024-06-01 16:43:15 +0530

    add tailwindcss and bug fixes

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,17 @@ import Link from "next/link";
 
 function index() {
   return (
-    <main className="bg-gray-200 min-h-screen w-full">
-      <h1 className="text-4xl font-semibold text-center pt-16">React Projects</h1>
+    <main className="bg-gray-200 h-screen w-full">
       <ul className="grid grid-cols-1 md:grid-cols-3 place-items-center px-6 md:px-40 gap-10 md:gap-20 pt-16">
         <Link
           href="/review"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
+          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
         >
           Review
         </Link>
         <Link
-          href="/sideBar"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
+          href="/question"
+          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
         >
           {" "}
           SideBar
@@ -38,28 +37,21 @@ function index() {
         {" "}
         <Link
           href="/menu"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
+          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
         >
           {" "}
           Menu
         </Link>
         <Link
           href="/paragraph"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
+          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
         >
           {" "}
           Paragraph
         </Link>
         <Link
-          href="/question"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {" "}
-          Question
-        </Link>
-        <Link
-          href="/navbar"
-          className="border-2 text-2xl font-semibold flex items-center justify-center hover:bg-gray-100 bg-white shadow-xl w-full h-60 rounded-xl"
+          href="/sideBar"
+          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
         >
           {" "}
           Navbar
