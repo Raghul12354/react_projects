fc080f4fc7caada15003983211a9a9f6e0eb7358
raghul <raghulkrishna50@gmail.com>
2024-06-23 15:27:49 +0530

    fix: responsiveness for review component

--- components/reviews/section.js
+++ components/reviews/section.js
@@ -37,18 +37,18 @@ const section = () => {
         setIndex(checkNumber(randomNumber));
     }
     return (
-        <div className="flex flex-col">
-            <div className="bg-white box-border shadow-md w-[550px] mx-auto rounded-md my-6 px-8 py-5">
+        <div className="grid place-items-center px-8">
+            <div className="bg-white box-border shadow-md w-full sm:w-[550px] rounded-md my-6 px-8 py-5">
                 <div className="flex flex-col items-center justify-center">
-                    <img className="rounded-full h-[150px] w-[150px] object-cover" src={image} alt={name} />
-                    <h2 className="text-lg font-medium capitalize mt-1">{name}</h2>
-                    <h4 className="text-blue text-base font-semibold capitalize tracking-wider mt-1">{job}</h4>
-                    <p className="text-center text-base text-gray-500 mb-3 w-[85%]">{text}</p>
+                    <img className="rounded-full h-[100px] w-[100px] md:h-[150px] md:w-[150px] object-cover" src={image} alt={name} />
+                    {/* <h2 className="text-lg font-medium capitalize mt-2">{name}</h2> */}
+                    <h4 className="text-blue text-lg md:text-base font-semibold capitalize tracking-wider mt-2">{job}</h4>
+                    <p className="text-center text-sm md:text-base text-gray-500 mb-3 w-[85%] text-balance">{text}</p>
                     <div className=' flex flex-row items-center justify-center mx-auto'>
-                        <button onClick={prev} className=' mx-4 text-blue text-2xl'>
+                        <button onClick={prev} className='mx-4 text-blue text-2xl'>
                             <Btn />
                         </button>
-                        <button onClick={next} className=' mx-4 text-blue text-2xl '>
+                        <button onClick={next} className='mx-4 text-blue text-2xl'>
                             <BtnNext />
                         </button>
                     </div>
