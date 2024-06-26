0ec5b142ba8c0b239af9a86681f46c20a6c3cc67
raghul <raghulkrishna50@gmail.com>
2024-06-26 19:09:18 +0530

    add: images, reusable code

--- pages/index.js
+++ pages/index.js
@@ -19,49 +19,57 @@ import Image from "next/image";
 // import Clone from "../components/cart-project/Clone-cart/clone";
 
 function index() {
+
+  const dataMapping = [
+    {
+      id: 1,
+      img: "/review_bg.png",
+      title: "Review",
+      link: "/review",
+    },
+    {
+      id: 2,
+      img: "/question_bg.png",
+      title: "Accordion",
+      link: "/question"
+    },
+    {
+      id: 3,
+      img: "/menu_bg.png",
+      title: "Menu",
+      link: "/menu"
+    },
+    {
+      id: 4,
+      img: "/para_bg.png",
+      title: "Paragraph",
+      link: "/paragraph"
+    },
+    {
+      id: 5,
+      img: "/sidebar_bg.png",
+      title: "SideBar & Model",
+      link: "/sidebar"
+    }
+  ]
   return (
     <main className="bg-gray-200 min-h-screen w-full pb-10">
       <div className="flex justify-center items-center flex-col mb-[3rem]">
         <h1 className="text-[2.5rem] font-bold text-blacky  tracking-wide capitalize mt-20">React Projects</h1>
         <div className="mx-auto h-1 w-24 bg-blue mt-2"></div>
       </div>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center px-6 md:px-20 xl:px-40 gap-10 md:gap-20 pt-16">
-        <Link
-          href="/review"
-          className="relative border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {/* <Image src="/review_bg.png" alt="" fill={true} sizes="" className="object-cover object-center" /> */}
-          Review
-        </Link>
-        <Link
-          href="/question"
-          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {" "}
-          Accordion
-        </Link>
-        {" "}
-        <Link
-          href="/menu"
-          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {" "}
-          Menu
-        </Link>
-        <Link
-          href="/paragraph"
-          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {" "}
-          Paragraph
-        </Link>
-        <Link
-          href="/sideBar"
-          className="border-2 text-2xl font-semibold flex items-center justify-center bg-white shadow-xl w-full h-60 rounded-xl"
-        >
-          {" "}
-          SideBar
-        </Link>
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center px-6 md:px-10 xl:px-28 gap-10 md:gap-20 pt-16">
+
+        {dataMapping.map((item) => (
+          <Link
+            key={item.id}
+            href={item.link}
+            className="border-2 bg-white shadow-xl w-full min-h-80 rounded-xl"
+          >
+            <Image src={item.img} alt={item.title} width={640} height={434} className="object-cover object-center w-full h-80" />
+            <p className="text-center py-3 text-xl md:text-2xl font-medium">{item.title}</p>
+          </Link>
+        ))}
       </ul>
 
       {/* <Birthday /> works */}
