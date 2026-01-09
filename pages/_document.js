06c97cc4a634085d092536faa8b17175aad11a48
raghul <raghulkrishna50@gmail.com>
2026-01-09 19:51:03 +0530

    chore: update repo with small fixes

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          fcm_service_path="/firebase-messaging-sw.js"
-          src="https://sdk.smartdx.co/handlers/2bdae61e_7d8c_432b_9815_ac4d1c8c82e2.sdk"
-          defer="defer"
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
